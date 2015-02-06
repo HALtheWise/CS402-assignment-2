8d443112c5166fb70f3d3d966044dbcacb9e922a
Eric Miller <eric@legoaces.org>
2015-02-06 01:52:29 -0500

    Got score display working

--- Assets/Scripts/Character.js
+++ Assets/Scripts/Character.js
@@ -5,13 +5,24 @@ var isMoving:boolean = false;
 var explosion:GameObject;
 var explosionSound:AudioClip;
 
+var numCatches:int;
+var bestScore:int = 0;
+var health:int = 100;
+var lastBest:int = 0;
+var hasLost:boolean = false;
+var catchCounter:GUIText;
+
 function Start()
 {
+	numCatches = 0;
+	hasLost = false;
 	animation.Stop(); // prevent default animation
 }
+
 function Update()
 {
-	var halfW:float = Screen.width / 2;
+	updateDisplays();
+
 	transform.position.x = (Input.mousePosition.x)/Screen.width * 150 - 50;
 	
 	if(lastX != transform.position.x){ //Mouse is moving
@@ -33,6 +44,30 @@ function Update()
 	lastX = transform.position.x;
 }
 
+function updateDisplays(){
+	var str:String = "";
+	
+	if(!hasLost){
+		str = numCatches.ToString();
+	}
+	else {
+		str = "Hits:" + numCatches.ToString() + "\nYour best:" +
+		bestScore;
+		if(bestScore > lastBest) str += "\nNEW RECORD!";
+	}
+	catchCounter.text = str;
+	
+	if(health <= 0){
+		if(!hasLost) {
+			hasLost = true;
+			lastBest = bestScore;
+			if(numCatches > bestScore) {
+				bestScore = numCatches;
+			}
+		}
+	}
+}
+
 function OnCollisionEnter(col : Collision)
 {
 	if(col.gameObject.tag == "bomb")
@@ -43,6 +78,7 @@ function OnCollisionEnter(col : Collision)
 	else if (col.gameObject.tag == "stein")
 	{
 		animation.Play("catch");
+		numCatches++;
 	}
 	col.gameObject.transform.position.y = 50;
 	col.gameObject.transform.position.x = Random.Range(0,60);
