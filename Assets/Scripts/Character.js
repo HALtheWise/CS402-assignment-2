b7cac9a6955507f1c7b084bc18b6d0b31db9f689
Eric Miller <eric@legoaces.org>
2015-02-05 22:43:09 -0500

    Character motion animation added

--- Assets/Scripts/Character.js
+++ Assets/Scripts/Character.js
@@ -1,9 +1,27 @@
 ﻿#pragma strict
 
-function Start () {
+var lastX:float;
+var isMoving:boolean = false;
 
+function Start()
+{
+	animation.Stop(); // prevent default animation
 }
-
-function Update () {
-
+function Update()
+{
+	var halfW:float = Screen.width / 2;
+	transform.position.x = (Input.mousePosition.x)/20;
+	
+	if(lastX != transform.position.x){ //Mouse is moving
+		
+		if (!isMoving){
+			isMoving = true;
+			animation.Play("step");
+		}
+	}else{
+		if (isMoving){
+			isMoving = false;
+			animation.Play("idle");
+		}		
+	}
 }
