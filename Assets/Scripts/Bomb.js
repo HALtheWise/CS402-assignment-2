bfae724a246634a24085197c4a3a8f1c701f117b
Eric Miller <eric@legoaces.org>
2015-02-05 23:16:41 -0500

    End chapter: The Breakup part 1

--- Assets/Scripts/Bomb.js
+++ Assets/Scripts/Bomb.js
@@ -1,9 +1,17 @@
 ﻿#pragma strict
 
+var explosion:GameObject;
 function Start () {
 
 }
 
-function Update () {
-
+function Update(){
+	transform.position.y -= 50 * Time.deltaTime;
+	if(transform.position.y <= 0)
+	{
+		Instantiate(explosion, transform.position, Quaternion.identity);
+		transform.position.y = 50;
+		transform.position.x = Random.Range(0,60);
+		transform.position.z = -16;
+	}
 }
